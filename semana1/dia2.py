6a5b58128b0951f198e83899e2f704433b2255b0
DanielaMald <122801429+DanielaMald@users.noreply.github.com>
2023-02-06 06:01:59 +0000

    Initial commit

---
+++ semana1/dia2.py
@@ -0,0 +1,15 @@
+"""
+    Programa2
+    Nombre: Dulce Daniela Maldonado Moctezuma
+    Fecha: 02/02/23
+    Descripcion: Ejercicios de practica
+
+ """
+
+print("Suma de dos cantidades")#imprime en la pantalla para identificarlo
+numero_1 = int(input(" escribe cualquier numero: "))#el valor del numero1
+numero_2 = int(input("escribe cualquier otro numero: "))#valor del numero2
+print("el resultado de la suma es: ")#imprime el texto 
+print= (input(numero_1 + numero_2))#realiza la operacion 
+
+
