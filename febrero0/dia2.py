598f276ea4041af76171ffe3e529ca8490714e7a
DanielaMald <122801429+DanielaMald@users.noreply.github.com>
2023-02-07 13:33:17 +0000

    cambios

--- febrero0/dia2.py
+++ febrero0/dia2.py
@@ -9,7 +9,8 @@
 print("Suma de dos cantidades")#imprime en la pantalla para identificarlo
 numero_1 = int(input(" escribe cualquier numero: "))#el valor del numero1
 numero_2 = int(input("escribe cualquier otro numero: "))#valor del numero2
-print("el resultado de la suma es: ")#imprime el texto 
-print= (input(numero_1 + numero_2))#realiza la operacion 
+suma = numero_1 + numero_2 #realiza la operacion 
+print("el resultado de la suma es: ", suma)#imprime el texto 
+
 
 
