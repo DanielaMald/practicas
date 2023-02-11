f0601e52070ada46fc0f2809f2975150889a003c
DanielaMald <122801429+DanielaMald@users.noreply.github.com>
2023-02-11 23:06:19 +0000

    ejercicio de practica. suma de dos lados

---
+++ febrero0/dia3.py
@@ -0,0 +1,12 @@
+"""
+    Programa3
+    Nombre: Dulce Daniela Maldonado Moctezuma
+    Fecha: 03/02/23
+    Descripcion: Ejercicios de practica
+
+ """
+print("Suma de dos lados")#imprime en la pantalla para identificarlo
+lado_1 = int(input(" escribe cualquier medida: "))#el valor del numero1
+lado_2 = int(input("escribe otra medida: "))#valor del numero2
+suma = lado_1 + lado_2 #realiza la operacion 
+print("el resultado de la suma es: ", suma)#imprime el texto 
---
+++ febrero0/dia4.py
@@ -0,0 +1,8 @@
+"""
+    Programa4
+    Nombre: Dulce Daniela Maldonado Moctezuma
+    Fecha: 04/02/23
+    Descripcion: Ejercicios de practica
+
+ """
+
